e3fe80f28cae4bdc0383c16329e3c9c20009a6bd
Ethemkk <you@example.com>
2024-09-05 22:59:26 +0300

    navbar finish

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,8 +2,16 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div>
+    <div className="flex justify-between bg-stone-700 py-5">
       <img src="/logo.png" alt="" />
+      <div className="flex gap-4 mr-4">
+        <button className="text-white bg-blue-400 rounded-md p-2">
+          Giriş Yap
+        </button>
+        <button className="text-white bg-blue-400 rounded-md p-2">
+          Kayıt Ol
+        </button>
+      </div>
     </div>
   );
 };
